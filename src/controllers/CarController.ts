7e512d283b04d840f407283543fe6b58de2c0361
Danielle Cristina <dnaielle.trybe@gmail.com>
2022-08-19 17:15:54 -0300

    feat: :sparkles: criando CarController

---
+++ src/controllers/CarController.ts
@@ -0,0 +1,24 @@
+import { Request, Response } from 'express';
+import IService from '../interfaces/IService';
+import { ICar } from '../interfaces/ICar';
+
+export default class CarController {
+  constructor(private _service: IService<ICar>) {}
+
+  public async create(req: Request & { body: ICar }, res: Response<ICar>) {
+    const { model, year, color, buyValue, seatsQty, doorsQty } = req.body;
+    const Car = { model, year, color, buyValue, seatsQty, doorsQty };
+    const result = await this._service.create(Car);
+    return res.status(201).json(result);
+  }
+
+  public async read(_req: Request, res: Response<ICar[]>) {
+    const result = await this._service.read();
+    return res.status(200).json(result);
+  }
+
+  public async readOne(req: Request, res: Response<ICar>) {
+    const result = await this._service.readOne(req.params.id);
+    return res.status(200).json(result);
+  }
+}
